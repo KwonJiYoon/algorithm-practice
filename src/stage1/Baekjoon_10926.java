2186b2ccb25a02a4f7b30694a07d5c02a2d11d1a
jykwon <jykwon@12cm.co.kr>
2022-04-07 14:05:00 +0900

    1단계 완료

---
+++ src/stage1/Baekjoon_10926.java
@@ -0,0 +1,11 @@
+package stage1;
+
+import java.util.Scanner;
+
+public class Baekjoon_10926 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String id = sc.next();
+        System.out.println(id + "??!");
+    }
+}
