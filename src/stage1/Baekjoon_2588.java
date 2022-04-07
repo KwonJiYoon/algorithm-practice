91d0fda6ce1f85a62cc95384c80640c56d4df30f
jykwon <jykwon@12cm.co.kr>
2022-04-07 14:10:48 +0900

    1단계 완료

--- src/stage1/Baekjoon_2588.java
+++ src/stage1/Baekjoon_2588.java
@@ -11,6 +11,8 @@ public class Baekjoon_2588 {
         int a = Integer.parseInt(br.readLine());
         int b = Integer.parseInt(br.readLine());
 
+
+
         br.close();
 
         System.out.println(a * (b%10));
