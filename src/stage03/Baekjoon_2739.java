f7cf22042fc99c2f8d3c0c64423edf822b8c60e5
jykwon <jykwon@12cm.co.kr>
2022-10-30 11:01:12 +0900

    백준 2단계 - 2739번

---
+++ src/stage03/Baekjoon_2739.java
@@ -0,0 +1,24 @@
+package stage03;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 2022.10.30 (일)
+
+// N을 입력받은 뒤, 구구단 N단을 출력하는 프로그램을 작성하시오. 출력 형식에 맞춰서 출력하면 된다.
+// 첫째 줄에 N이 주어진다. N은 1보다 크거나 같고, 9보다 작거나 같다.
+
+public class Baekjoon_2739 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine());
+
+        for (int i = 1; i < 10; i ++) {
+            System.out.println(N + " * " + i + " = " + N * i);
+        }
+    }
+}
+
+//========================= memo ==========================//
+
