012b53399c9a8ada7116ada5dcb787f2b1e0a178
KwonJiYoon <60695604+KwonJiYoon@users.noreply.github.com>
2024-06-13 08:56:10 +0900

    Time: 9 ms (6.75%), Space: 43.3 MB (9.14%) - LeetHub

--- LeetCode/Medium/0075-sort-colors/0075-sort-colors.java
+++ LeetCode/Medium/0075-sort-colors/0075-sort-colors.java
@@ -1,17 +1,42 @@
 class Solution {
     public void sortColors(int[] nums) {
-        
-        int length = nums.length - 1;
-        
-        for(int i = 0; i < length; i++) {
-            for(int j = 0; j < length - i; j++) {
-                int temp = 0;
-                if(nums[j] > nums[j + 1]) {
-                    temp = nums[j];
-                    nums[j] = nums[j + 1];
-                    nums[j + 1] = temp;
-                }
+
+        int r = 0;
+        int w = 0;
+        int b = nums.length - 1;
+
+        // nums = [2,0,2,1,1,0]
+        // b = 5
+        // w <= 5
+        while (w <= b) {
+            if (nums[w] == 0) {
+                swap(r++, w++, nums);
+            } else if (nums[w] == 1) {
+                w++;
+            } else {
+                swap(w, b--, nums);
             }
         }
     }
+
+    public void swap(int i, int j, int[] nums) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+
+        System.out.println(Arrays.toString(nums));
+    }
+
+    // w : 0  / b : 5
+    // num[0] : 2 > else > swap(0, 5, nums) 
+    // i : 0, j : 5
+    // temp = nums[0] = 2 // nums[0] = nums[5] = 0 // nums[5] = 2
+    // [0, 0, 2, 1, 1, 2]
+
+    // w : 0  / b : 4
+    // num[0] : 0 > if > swap(0, 0, nums) 
+    // i : 0, j : 0
+    // temp = nums[0] = 0 // nums[0] = nums[0] = 0 // nums[0] = 0
+    // [0, 0, 2, 1, 1, 2]
+
 }
