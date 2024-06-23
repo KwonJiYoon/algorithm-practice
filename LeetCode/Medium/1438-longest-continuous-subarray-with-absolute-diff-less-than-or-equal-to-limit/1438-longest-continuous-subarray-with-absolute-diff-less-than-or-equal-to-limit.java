cb01c3a047e9574f7ae757915ea0241fb6ce786f
KwonJiYoon <60695604+KwonJiYoon@users.noreply.github.com>
2024-06-23 12:18:43 +0900

    Time: 31 ms (66.25%), Space: 56.5 MB (57.47%) - LeetHub

--- LeetCode/Medium/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
+++ LeetCode/Medium/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -3,11 +3,11 @@ import java.util.LinkedList;
 
 public class Solution {
     public int longestSubarray(int[] nums, int limit) {
-        // 슬라이딩 윈도우의 양 끝을 유지하는 덱
+        // 최대값과 최소값을 저장하는 덱
         Deque<Integer> maxDeque = new LinkedList<>();
         Deque<Integer> minDeque = new LinkedList<>();
-        int left = 0;
-        int maxLength = 0;
+        int left = 0; // 슬라이딩 윈도우의 왼쪽 끝
+        int maxLength = 0; // 조건을 만족하는 가장 긴 부분 배열의 길이
 
         for (int right = 0; right < nums.length; right++) {
             // maxDeque 업데이트: 현재 값보다 작은 값 제거
@@ -22,9 +22,9 @@ public class Solution {
             }
             minDeque.addLast(right);
 
-            // 윈도우 조건 확인 및 필요 시 축소
+            // 윈도우 내의 최대값과 최소값의 차이가 limit을 초과하면 윈도우 축소
             while (nums[maxDeque.peekFirst()] - nums[minDeque.peekFirst()] > limit) {
-                left++;
+                left++; // 윈도우의 왼쪽 끝을 오른쪽으로 이동
                 if (maxDeque.peekFirst() < left) {
                     maxDeque.pollFirst();
                 }
@@ -33,7 +33,7 @@ public class Solution {
                 }
             }
 
-            // 최대 길이 갱신
+            // 조건을 만족하는 현재 윈도우의 길이를 계산하여 최대 길이 갱신
             maxLength = Math.max(maxLength, right - left + 1);
         }
 
