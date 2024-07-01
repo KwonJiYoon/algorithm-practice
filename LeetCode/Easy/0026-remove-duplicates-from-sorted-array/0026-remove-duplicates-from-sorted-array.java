1c43a84c1a13b65f6de63b91d13132e56e392df4
KwonJiYoon <60695604+KwonJiYoon@users.noreply.github.com>
2024-07-01 21:54:25 +0900

    Time: 0 ms (100%), Space: 44.4 MB (91.17%) - LeetHub

--- LeetCode/Easy/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ LeetCode/Easy/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -6,7 +6,6 @@ class Solution {
         for (int i = 1; i < nums.length; i ++) {
 
             if (nums[i] != nums[i - 1]) {
-                System.out.println(nums[index] + "/" + nums[i]);
                 nums[index] = nums[i];
                 index ++;
             }
