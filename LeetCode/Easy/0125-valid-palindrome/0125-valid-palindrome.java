4ae66ddeb40d06b9c5b8934b49b65f4a9fe2ad5e
KwonJiYoon <60695604+KwonJiYoon@users.noreply.github.com>
2024-07-03 22:26:48 +0900

    Time: 3 ms (73.02%), Space: 42.5 MB (95.56%) - LeetHub

--- LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.java
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,26 +1,29 @@
 class Solution {
     public boolean isPalindrome(String s) {
-    
-        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        
+        // Initialize pointers
+        int left = 0;
+        int right = s.length() - 1;
 
-        int start = 0;
-        int end = s.length() - 1;
-        
-        if (s.length() == 0) {
-            return true;
-        }
+        while (left < right) {
+            // Move the left pointer to the next alphanumeric character
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
 
-        while(start < end) {
-            if(s.charAt(start) == s.charAt(end)) {
-                start++;
-                end--;
-            } else {
+            // Move the right pointer to the previous alphanumeric character
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            // Compare the characters at the left and right pointers
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
                 return false;
             }
-            
+
+            // Move both pointers towards the center
+            left++;
+            right--;
         }
-        
 
         return true;
     }
