a13b9f2b879e8c43861a99a1b470f6d3a99d639f
KwonJiYoon <60695604+KwonJiYoon@users.noreply.github.com>
2024-08-20 21:07:35 +0900

    Time: 0 ms (100%), Space: 42.1 MB (5.97%) - LeetHub

--- LeetCode/Easy/0058-length-of-last-word/0058-length-of-last-word.java
+++ LeetCode/Easy/0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,13 +1,17 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] word = s.split(" ");
+        String word = s.trim();
+        int count = 0;
 
-        for(int i = word.length - 1; i >= 0; i --) {
-            if(!word[i].isBlank()) {
-                return word[i].length();
+        for(int i = word.length() - 1; i >= 0; i --) {
+            if(word.charAt(i) != ' ') {
+                count++;
+            }
+            else if (count > 0) {
+                return count;
             }
         }
 
-        return 0;
+        return count;
     }
 }
