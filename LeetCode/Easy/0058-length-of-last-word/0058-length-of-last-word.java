21d0e670606ea504e6e16a1d25f18e27582ad060
KwonJiYoon <60695604+KwonJiYoon@users.noreply.github.com>
2024-08-20 21:08:39 +0900

    Time: 0 ms (100%), Space: 41.7 MB (30.46%) - LeetHub

--- LeetCode/Easy/0058-length-of-last-word/0058-length-of-last-word.java
+++ LeetCode/Easy/0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,10 +1,10 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String word = s.trim();
+        
         int count = 0;
 
-        for(int i = word.length() - 1; i >= 0; i --) {
-            if(word.charAt(i) != ' ') {
+        for(int i = s.length() - 1; i >= 0; i --) {
+            if(s.charAt(i) != ' ') {
                 count++;
             }
             else if (count > 0) {
