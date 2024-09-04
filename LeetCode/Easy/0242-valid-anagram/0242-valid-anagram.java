a3c90c342e08e8809fb154257a03475948d4bd87
KwonJiYoon <60695604+KwonJiYoon@users.noreply.github.com>
2024-09-04 21:37:44 +0900

    Time: 23 ms (8.36%), Space: 44.5 MB (48.72%) - LeetHub

--- LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.java
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.java
@@ -1,25 +1,21 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> sh = new HashMap<>();
+        HashMap<Character, Integer> th = new HashMap<>();
+        Set<Character> set = new HashSet<>();
 
-        Map<Character, Integer> map = new HashMap<>();
-
-        if(s.length() != t.length()) return false;
-
-        for(int i = 0; i < s.length(); i++) {
-            char sChar = s.charAt(i);
-            map.put(sChar, map.getOrDefault(sChar, 0) + 1);
+        for (int i = 0; i < s.length(); ++i) {
+            sh.put(s.charAt(i), sh.getOrDefault(s.charAt(i), 0) + 1);
+            set.add(s.charAt(i));
+        }
+        for (int i = 0; i < t.length(); ++i) {
+            th.put(t.charAt(i), th.getOrDefault(t.charAt(i), 0) + 1);
+            set.add(t.charAt(i));
         }
 
-        for(int j = 0; j < t.length(); j++) {
-            char tChar = t.charAt(j);
-            if(!map.containsKey(tChar) || map.get(tChar) == 0) {
-                return false;
-            } else {
-                map.put(tChar, map.get(tChar) - 1);
-            }
-            
+        for (Character key : set) {
+            if ((int)sh.getOrDefault(key, 0) != (int)th.getOrDefault(key, 0)) return false;
         }
-        
         return true;
     }
 }
