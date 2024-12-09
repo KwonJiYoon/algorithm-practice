0226abe5a6abc1d8bcdc2eb65ac4ddebf01d64f4
KwonJiYoon <60695604+KwonJiYoon@users.noreply.github.com>
2024-12-09 10:29:47 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 5.30 ms, Memory: 90 MB -BaekjoonHub

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.java
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.java
@@ -2,7 +2,8 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
 
-        for(int i = 1; i <= n / 2; i++) {
+        // i의 초기값이 1인 경우 불필요한 연산 발생 :: 어떤 수 n에 대해 n % 1은 항상 0
+        for(int i = 2; i < n / 2; i++) {
             if(n % i == 1) {
                 answer = i;
                 break;
