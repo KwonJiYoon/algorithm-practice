21e18abd6ed215532d135848a16dc70711b4d49a
KwonJiYoon <60695604+KwonJiYoon@users.noreply.github.com>
2024-12-30 17:24:35 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 83.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        long target = Long.valueOf(num);
+        
+        while(target != 1) {
+            if(target % 2 == 0) {
+                target = target / 2;
+            } else {
+                target = target * 3 + 1;
+            }
+            answer++;
+            
+            if(answer == 500) {
+                return -1;
+            }
+        }
+        return answer;
+    }
+}
