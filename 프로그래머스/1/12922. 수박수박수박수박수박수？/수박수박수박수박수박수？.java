b09d621b1278d5b50119d1fd07bcd4a0af81b15d
KwonJiYoon <60695604+KwonJiYoon@users.noreply.github.com>
2025-01-06 22:24:01 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.12 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,12 +1,17 @@
 class Solution {
     public String solution(int n) {
         
-        StringBuilder answer = new StringBuilder();
+//         StringBuilder answer = new StringBuilder();
         
-        for(int i = 1; i <= n; i++) {
-            answer.append(i % 2 != 0 ? "수" : "박");
-        }
+//         for(int i = 1; i <= n; i++) {
+//             answer.append(i % 2 != 0 ? "수" : "박");
+//         }
         
-        return answer.toString();
+//         return answer.toString();
+        
+        
+        String word = "수박".repeat(n / 2 + 1);
+        return word.substring(0, n);
+        
     }
 }
