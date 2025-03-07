b14403ac4f04bb3f75276ea85dc7acba969575e3
KwonJiYoon <60695604+KwonJiYoon@users.noreply.github.com>
2025-03-07 14:30:02 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.94 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -1,17 +1,23 @@
 class Solution {
     public int solution(String t, String p) {
-        int length = t.length() - p.length() + 1;
+        int pLength = p.length();
+        long pToNum = Long.parseLong(p);
         int result = 0;
+        
+        long tempNum = 0;
+        
+        // 첫 번째 부분 문자열 숫자로 변환
+        for (int i = 0; i < pLength; i++) {
+            tempNum = tempNum * 10 + (t.charAt(i) - '0');
+        }
 
-        for (int i = 0; i < length; i++) {
+        // 첫 번째 숫자 비교
+        if (tempNum <= pToNum) result++;
 
-            String tempNum = t.substring(i, i + p.length());
-
-            if (Long.parseLong(tempNum) <= Long.parseLong(p)) {
-                result ++;
-            }
-
-
+        // 슬라이딩 윈도우 기법 사용
+        for (int i = pLength; i < t.length(); i++) {
+            tempNum = (tempNum % (long) Math.pow(10, pLength - 1)) * 10 + (t.charAt(i) - '0');
+            if (tempNum <= pToNum) result++;
         }
         
         return result;
