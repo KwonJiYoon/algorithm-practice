f7ec5d41e4f5c68c8a2b8c40e8d60bbc3888ee72
KwonJiYoon <60695604+KwonJiYoon@users.noreply.github.com>
2025-03-19 22:20:32 +0900

    [level 1] Title: 시저 암호, Time: 2.30 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,16 +1,13 @@
 class Solution {
     public String solution(String s, int n) {
-        StringBuilder answer = new StringBuilder();
+        StringBuilder answer = new StringBuilder(s.length());
 
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-
+        for (char ch : s.toCharArray()) {
             if (Character.isLowerCase(ch)) {
                 ch = (char) ('a' + (ch - 'a' + n) % 26);
             } else if (Character.isUpperCase(ch)) {
-                ch = (char) ('A' + (ch - 'A' + n) % 26); 
+                ch = (char) ('A' + (ch - 'A' + n) % 26);
             }
-
             answer.append(ch);
         }
 
