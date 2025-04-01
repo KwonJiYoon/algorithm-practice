b491168f96b97b1c36a6b05f9e75944adcaa014b
KwonJiYoon <60695604+KwonJiYoon@users.noreply.github.com>
2025-04-01 10:12:18 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 1.39 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -5,7 +5,7 @@ class Solution {
         
         for(int i = 0; i < arr.length; i++) {
             if(s.contains(arr[i])) {
-                s = s.replace(arr[i], Integer.toString(i));
+                s = s.replaceAll(arr[i], Integer.toString(i));
             }
         }
         
