312c0c920bd61a902d30d844cc30fba42a24ee24
KwonJiYoon <60695604+KwonJiYoon@users.noreply.github.com>
2025-04-02 09:52:03 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 15.51 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,19 +1,13 @@
 class Solution {
     public String solution(int[] food) {
         StringBuilder left = new StringBuilder();
-        StringBuilder right = new StringBuilder();
-        int num = 1;
-
+        
         for (int i = 1; i < food.length; i++) {
             int count = food[i] / 2;
             for (int j = 0; j < count; j++) {
-                left.append(num);
-                right.insert(0, num);
+                left.append(i);
             }
-            
-            num++;
         }
-
-        return left.toString() + "0" + right.toString();
+        return left.toString() + "0" + left.reverse().toString();
     }
 }
