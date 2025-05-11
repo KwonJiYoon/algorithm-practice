f18c14fec896f9ed82167ce55ff86b4aa4c97aaf
KwonJiYoon <60695604+KwonJiYoon@users.noreply.github.com>
2025-05-11 21:28:37 +0900

    [level 1] Title: 폰켓몬, Time: 6.59 ms, Memory: 97.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,13 +2,16 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
-        Map<Integer, Integer> map = new HashMap<>();
-        
-        for(int n : nums) {
-            map.put(n, map.getOrDefault(n, 0) + 1);    
-        }
-        
-        return nums.length / 2 > map.size() ? map.size() : nums.length / 2;
+
+            HashSet<Integer> hs = new HashSet<>();
+
+            for(int i =0; i<nums.length;i++) {
+                hs.add(nums[i]);
+            }
+
+            if(hs.size()>nums.length/2)
+                return nums.length/2;
+
+            return hs.size();
     }
 }
