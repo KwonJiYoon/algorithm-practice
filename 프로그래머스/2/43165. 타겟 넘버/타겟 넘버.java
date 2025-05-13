bd05f712759dbb617c4a049593552b0f53e5958e
KwonJiYoon <60695604+KwonJiYoon@users.noreply.github.com>
2025-05-13 22:49:11 +0900

    [level 2] Title: 타겟 넘버, Time: 7.19 ms, Memory: 68.9 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,28 +1,21 @@
-import java.util.*;
-
 class Solution {
-    Map<String, Integer> memo = new HashMap<>();
+    int answer = 0;
 
     public int solution(int[] numbers, int target) {
-        return dfs(0, 0, numbers, target);
+        
+        dfs(0, numbers, target, 0);
+        
+        return answer;
     }
-
-    public int dfs(int depth, int sum, int[] numbers, int target) {
-        String key = depth + "," + sum; // 현재 상태를 기억할 key
-
-        if (memo.containsKey(key)) {
-            return memo.get(key); // 이미 계산했던 값이면 다시 계산 안 하고 return
+    
+    public void dfs(int depth, int[] numbers, int target, int sum) {
+        if(depth == numbers.length) {
+            if(sum == target) {
+                answer++;
+            }
+            return;
         }
-
-        if (depth == numbers.length) {
-            return sum == target ? 1 : 0; // 마지막까지 다 썼을 때 sum이 target이면 경우의 수 1
-        }
-
-        // 다음 숫자를 더하거나 빼는 두 가지 경우로 재귀 호출
-        int plus = dfs(depth + 1, sum + numbers[depth], numbers, target);
-        int minus = dfs(depth + 1, sum - numbers[depth], numbers, target);
-
-        memo.put(key, plus + minus); // 현재 상태 결과 저장
-        return plus + minus;
+        dfs(depth + 1, numbers, target, sum + numbers[depth]);
+        dfs(depth + 1, numbers, target, sum - numbers[depth]);
     }
 }
