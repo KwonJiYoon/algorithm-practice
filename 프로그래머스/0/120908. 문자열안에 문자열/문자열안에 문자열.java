6b0bac479f92fa7fed6b8c03b1a78d89cfc056f2
KwonJiYoon <60695604+KwonJiYoon@users.noreply.github.com>
2026-08-20 12:11:55 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.02 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -1,8 +1,5 @@
 class Solution {
     public int solution(String str1, String str2) {
-        int index = str1.indexOf(str2);
-        
-        
-        return index > -1 ? 1 : 2;
+        return str1.contains(str2) ? 1 : 2;
     }
 }
